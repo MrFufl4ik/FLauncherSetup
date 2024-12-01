5057b18416e1f27da8e5a71067a5f09d9d4db2df
MrFufl4ik <mrfufl4ik@gmail.com>
2024-12-01 16:42:34 +0600

    fixes

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ MC_RUN_LIB = "--user portablemc[certifi]"
 LAUNCHER_PATH = "C:\\FLauncher"
 LAUNCHER_GITHUB_URL = "https://github.com/MrFufl4ik/FLauncher.git"
 
+
 def is_windows():
     if os.name == "nt":
         return True
@@ -25,17 +26,23 @@ def install_python_libs(libs: list):
         os.system(f"{PIP_INSTALL_CMD} {lib}")
         print("Успешно")
 
+
 def git_clone(path: str):
     os.system(f"git clone {LAUNCHER_GITHUB_URL} {path}")
+
+
+def win_set_path(path: str):
+    os.system(f"setx PATH %PATH%;{path}");
+
 
 def win_install():
     print(f"Начало установки для: {os.name}")
     install_python_libs(BEGIN_LIBS)
     print("Начальные библиотеки установлены...")
     install_python_libs([MC_RUN_LIB])
+    win_set_path(f"C:\\Users\\{os.getlogin()}\\AppData\\Roaming\\Python\\Python312\\Scripts")
     os.makedirs(LAUNCHER_PATH)
     git_clone(LAUNCHER_PATH)
-
 
 
 def main():
